789356abf751bc37857145eedc056b1058e36957
Cutler Sheridan <cutler.sheridan@gmail.com>
2023-03-31 13:40:22 -0700

    0.2.1 Write tweet constructor; Add TweetCard component

---
+++ src/components/tweets/TweetCard.js
@@ -0,0 +1,36 @@
+import './TweetCard.css';
+// import { UserContext } from '../../UserContext';
+// import { useContext } from 'react';
+
+const TweetCard = ({ tweet, userInfo = null }) => {
+  // const userAuth = useContext(UserContext);
+  const creationMilliseconds = tweet.creationDate.seconds * 1000;
+
+  return (
+    <div className="tweetCard-wrapper">
+      <div className="tweetCard-middleRow">
+        <div className="tweetCard-avi"></div>
+        <div className="tweetCard-tweetAndUserWrapper">
+          <div className="tweetCard-nameAndHandleWrapper">
+            <div className="tweetCard-displayName">
+              Name: {userInfo ? userInfo.displayName : 'no user'}
+            </div>
+            <div className="tweetCard-handleAndDate">
+              {userInfo ? '@' + userInfo.handle : 'no handle'} ·{' '}
+              {new Date(creationMilliseconds).toDateString()}
+            </div>
+          </div>
+          <div className="tweetCard-tweet">{tweet.tweet}</div>
+        </div>
+      </div>
+      <div className="tweetCard-bottomRow">
+        <div>Replies</div>
+        <div>Retweets</div>
+        <div>Likes</div>
+        <div>Share</div>
+      </div>
+    </div>
+  );
+};
+
+export default TweetCard;
